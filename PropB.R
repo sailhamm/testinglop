62c3e4b499d1ae7882c451042e3704afaea8113d
sailhamm <141382031+sailhamm@users.noreply.github.com>
2026-07-10 10:29:23 +0700

    Load libraries for Shiny application
    Added necessary libraries for Shiny application.

---
+++ PropB.R
@@ -0,0 +1,12 @@
+# ==============================================================================
+# 1. LOAD LIBRARIES
+# ==============================================================================
+library(shiny)
+library(bslib)
+library(leaflet)
+library(plotly)
+library(DT)
+library(shinycssloaders) 
+library(ggplot2)
+library(dplyr)
+library(scales) 
